6d36071e6d12c219481330fa55e35d24f320bc54
Marcus Herrmann <mail@marcus-herrmann.com>
2020-08-07 08:55:10 +0200

    Update exceptions

--- components/Exceptions.vue
+++ components/Exceptions.vue
@@ -3,14 +3,28 @@
     <div class="section__inner">
       <h2>Ausnahmen</h2>
 
-      <p>Die Richtlinie 2019/882 kennt das Konzept der "unzumutbaren Belastung", was bedeutet, dass die Zugänglichkeitsregeln der Richtlinie gelten,</p>
+      <p>Die Richtlinie 2019/882 kennt das Konzept der "unzumutbaren Belastung" (Artikel 14, 1b), was bedeutet, dass die Zugänglichkeitsregeln der Richtlinie gelten,</p>
 
       <ul class="text-list">
         <li>…es sei denn, sie würden das Wesen des Produkts bzw. der Dienstleistung selbst verändern.</li>
         <li>…es sei denn, die Anforderungen an die Zugänglichkeit würden das Unternehmen finanziell überfordern.</li>
       </ul>
 
-        <p>Das bedeutet aber nicht, dass die Richtlinie große Lücken in ihrer Anwendbarkeit aufweist. Viele Unternehmen in der Europäischen Union werden vom Europäischen Gesetz zur Barrierefreiheit betroffen sein. <strong>Nur Kleinstunternehmen, d.h. Unternehmen, die weniger als 10 Personen beschäftigen und deren Jahresumsatz und/oder Jahresbilanzsumme 2 Millionen Euro nicht übersteigt, sind ausgenommen und können sich auf die "Befreiung von unzumutbarer Belastung" berufen.</strong>
+      <p>Den Unternehmen ("Wirtschaftsakteuren" in der Sprache der Direktive) ist es auferlegt, eine diesbezügliche Selbstbeurteilung vorzunehmen. Diese Beurteilung muss dokumentiert und für mindestens fünf Jahre (ab Zeitpunkt der Bereitstellung des Produktes oder der Dienstleistung) aufbewahrt werden. Fordern Marktüberwachungsbehörden (also jene Einrichtungen, die vom Mitgliedsstaat als Überwachungsstelle benannt werden) das Unternehmen dazu auf, muss diese Selbstbeurteilung an sie ausgehändigt werden.</p>
+
+      <p>Auf die "unzumutbare Belastung" nicht berufen lässt sich "wenn Wirtschaftsakteure zu Zwecken der Verbesserung der Barrierefreiheit nichteigene — öffentliche oder private — Mittel erhalten" (Artikel 14, 6).</p>
+
+      <p>Unternehmen, die Dienstleistungen erbringen, müssen die Selbstbeurteilung erneut ausführen…</p>
+
+      <ul class="text-list">
+        <li>…wenn die Überwachungsstelle sie dazu auffordert.</li>
+        <li>…wenn die angebotene Dienstleistung verändert wird.</li>
+        <li>…nach mindestens fünf Jahren.</li>
+      </ul>
+
+      <p>Das Konzept der "unzumutbaren Belastung" bedeutet aber nicht, dass die Richtlinie große Lücken in ihrer Anwendbarkeit aufweist. Denn: von Kleinstunternehmen, "die mit Produkten befasst sind" (Artikel 14, 4) abgesehen, ist sieht die Richtlinie eine schriftliche Rechtfertigung dafür vor, wenn man sich und sein Unternehmen als durch "Unzumutbarkeit" belastet und von der Richtline ausgenommen sind.</p>
+
+        <p>Es bleiben viele Unternehmen in der Europäischen Union übrig, die vom EAA betroffen sein werden. <strong>Denn nur Kleinstunternehmen, d.h. Unternehmen, die weniger als 10 Personen beschäftigen und deren Jahresumsatz und/oder Jahresbilanzsumme 2 Millionen Euro nicht übersteigt, können sich überhaupt auf die "Befreiung von unzumutbarer Belastung" berufen.</strong>
       </p>
     </div>
   </section>
