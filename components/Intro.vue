4e724bace0ebc1a17beb30b4ac4aaee435b54c93
Marcus Herrmann <mail@marcus-herrmann.com>
2021-03-24 16:00:50 +0100

    Fix position of Barrierefreiheitsstärkungsgesetz

--- components/Intro.vue
+++ components/Intro.vue
@@ -8,11 +8,11 @@
         <li>Die Richtlinie ist eine EU-weitere Vereinheitlichung der Vorschriften und Regularien zur Zugänglichkeit von Produkten und Dienstleistungen und zwingt die Anbieter, <strong>ihre Dienstleistungen und Produkte unter Berücksichtigung des <a href="https://www.fronta11y.org/inklusives-design-barrierefreiheit-richtlinien-fuer-designer/">inklusiven Designs</a> zu entwickeln und anzubieten.</strong></li>
         <li>Anders als zum Beispiel die Datenschutzgrundverordnung (DSGVO) ist sie jedoch eine <em>Richtlinie</em> und keine <em>Verordnung</em>. Zitat <a href="https://de.wikipedia.org/wiki/Richtlinie_(EU)">Wikipedia</a>:
 
-          <blockquote>Im Gegensatz zu Verordnungen gelten sie [die Richtlinien, Anm. d. Autors] gemäß Art. 288 Absatz 3 des AEUV <strong>nicht unmittelbar, sondern müssen erst von den Mitgliedstaaten in nationales Recht umgewandelt werden. In der Bundesrepublik Deutschland wird dies im "Barrierefreiheitsstärkungsgesetz" geschehen.</strong>
+          <blockquote>Im Gegensatz zu Verordnungen gelten sie [die Richtlinien, Anm. d. Autors] gemäß Art. 288 Absatz 3 des AEUV <strong>nicht unmittelbar, sondern müssen erst von den Mitgliedstaaten in nationales Recht umgewandelt werden.</strong>
           </blockquote>
 
           Das bedeutet: Eine Richtlinie wird nicht automatisch in allen EU-Mitgliedsstaaten Gesetz, sondern verpflichtet diese, den Richtlinien-Inhalt in nationales Recht zu verwandeln. Die dann entstandenen nationalen Gesetze sind die Rechtsordnungen, die letztlich relevant sind. Dennoch gibt die Richtlinie die Richtung vor.</li>
-
+          <li>In der Bundesrepublik Deutschland wird die Umsetzung der Richtlinie im "Barrierefreiheitsstärkungsgesetz" geschehen.</li>
           <li>Das bedeutet auch, dass weitere regulatorische Details und Handlungsempfehlungen für die Privatwirtschaft entstehen werden, sobald die nationalen Rechtsordnungen in Kraft sind. </li>
         <li>Nichtsdestotrotz ist <strong>genau jetzt</strong> ein guter Zeitpunkt, um über Zugänglichkeit und Barrierefreiheit der eigenen Dienstleistungen und Produkte nachzudenken, sofern das noch nicht geschen ist.</li>
         <li>Kritiker*innen beklagen, dass sich der European Accessibility Act zu sehr auf die digitale Welt konzentriert – und er nicht der große Wurf ist, der er hätte sein könnte. Dennoch ist die Richtlinie, nicht zuletzt wegen ihrer Auswirkungen auf die Europäische Union als Ganzes, ein großer Schritt nach vorn.</li>
